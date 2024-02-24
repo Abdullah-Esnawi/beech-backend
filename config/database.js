1acdffe84455ff98c27a91135bccdda590848160
Abdullah ahmed <abdullah.ahmed@hoods.live>
2024-02-24 17:47:05 +0200

    Fix Deployment issue

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = () => {
   mongoose
-    .connect(process.env.DB_URI)
+    .connect(process.env.DB_URI, {strictQuery: true})
     .then((conn) => {
       console.log(`Database Connected: ${conn.connection.host}`);
     })
